cf677b5ec5baf0e144815e18ff2f61aef02ef3ac
sebastianciocarlan <sebastianciocarlan@gmail.com>
2019-03-14 20:34:12 +0100

    fin libreria eclipse

--- LibreriaEclipse/src/libreriaEclipse/LibreriaEclipse.java
+++ LibreriaEclipse/src/libreriaEclipse/LibreriaEclipse.java
@@ -2,20 +2,43 @@ package libreriaEclipse;
 
 import java.util.Scanner;
 
+import librerias.Libreria;
+
 public class LibreriaEclipse {
 
 	public static void main(String[] args) {
 		Scanner sc  =new Scanner(System.in);
 		int entrada;
 		do {
+
 			System.out.println("------------------------------------------");
-			System.out.println("1- ");
-			System.out.println("2- ");
-			System.out.println("3- ");
-			System.out.println("4- ");
+			System.out.println("1- Factorial de un numero");
+			System.out.println("2- Tu nombre");
+			System.out.println("3- Valor maximo");
+			System.out.println("4- Salir ");
 			System.out.println("------------------------------------------");
 			entrada = sc.nextInt();
-
+			switch (entrada) {
+			case 1:
+				System.out.println("Escribe el numero del que sacaremos el factorial.");
+				Libreria.factorial(sc.nextInt());			
+				sc.nextLine();
+				break;
+			case 2:
+				System.out.println("Introduce tu nombre");
+				Libreria.escribirNombre(sc.nextLine());
+				break;
+			case 3:
+				System.out.println("Introduce el numero de numeros a evaluar");
+				Libreria.listaNumeros(sc.nextInt(), sc);
+				sc.nextLine();
+				break;
+			case 4:
+				break;
+			default:
+				System.out.println("Introduce un valor valido");
+				break;
+			}
 		}while(entrada!=4);
 		sc.close();
 	}
--- LibreriaEclipse/src/librerias/Libreria.java
+++ LibreriaEclipse/src/librerias/Libreria.java
@@ -1,5 +1,7 @@
 package librerias;
 
+import java.util.Scanner;
+
 public class Libreria {
 	public static void escribirNombre(String nombre) {
 		System.out.println("Hola mi nombre es : "+nombre);
@@ -11,5 +13,20 @@ public class Libreria {
 		}
 		System.out.println("El factorial es :"+valor);
 	}
-	
+	public static void listaNumeros(int n, Scanner sc) {
+		int[] array = new int[n];
+		
+		for(int i = 0; i<n;i++) {
+			System.out.println("Introduce el numero "+i+1);
+			array[i] = sc.nextInt();
+		}
+		int max = array[0];
+		for (int i =0; i<n;i++) {
+			if (array[i]>max) {
+				max = array[i];
+			}
+			
+		}
+		System.out.println("El mayor es: "+max);
+	}
 }
