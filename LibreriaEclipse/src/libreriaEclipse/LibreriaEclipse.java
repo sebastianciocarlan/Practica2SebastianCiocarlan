82c48ab6f5a963421305b622cc73ea18e6070900
Sebastian Ciocarlan <44316607+sebastianciocarlan@users.noreply.github.com>
2019-03-15 03:03:58 +0100

    revert

--- LibreriaEclipse/src/libreriaEclipse/LibreriaEclipse.java
+++ LibreriaEclipse/src/libreriaEclipse/LibreriaEclipse.java
@@ -7,37 +7,40 @@ import librerias.Libreria;
 public class LibreriaEclipse {
 
 	public static void main(String[] args) {
-
+		Scanner sc  =new Scanner(System.in);
 		int entrada;
 		do {
 
-			Console.Out.WriteLine("------------------------------------------");
-			Console.Out.WriteLine("1- Factorial de un numero");
-			Console.Out.WriteLine("2- Tu nombre");
-			Console.Out.WriteLine("3- Valor maximo");
-			Console.Out.WriteLine("4- Salir ");
-			Console.Out.WriteLine("------------------------------------------");
-			Console.Read() = entrada 
+			System.out.println("------------------------------------------");
+			System.out.println("1- Factorial de un numero");
+			System.out.println("2- Tu nombre");
+			System.out.println("3- Valor maximo");
+			System.out.println("4- Salir ");
+			System.out.println("------------------------------------------");
+			entrada = sc.nextInt();
 			switch (entrada) {
 			case 1:
-				Console.Out.WriteLine("Escribe el numero del que sacaremos el factorial.");
+				System.out.println("Escribe el numero del que sacaremos el factorial.");
 				Libreria.factorial(sc.nextInt());			
+				sc.nextLine();
 				break;
 			case 2:
-				Console.Out.WriteLine("Introduce tu nombre");
+				System.out.println("Introduce tu nombre");
 				Libreria.escribirNombre(sc.nextLine());
 				break;
 			case 3:
-				Console.Out.WriteLine("Introduce el numero de numeros a evaluar");
+				System.out.println("Introduce el numero de numeros a evaluar");
 				Libreria.listaNumeros(sc.nextInt(), sc);
+				sc.nextLine();
 				break;
 			case 4:
 				break;
 			default:
-				Console.Out.WriteLine("Introduce un valor valido");
+				System.out.println("Introduce un valor valido");
 				break;
 			}
 		}while(entrada!=4);
+		sc.close();
 	}
 
 }
